9bbcb1b459fb399e283ec686394319a143cf0ed7
Wyeha <snovavzloman@mail.ru>
2023-09-11 01:30:00 +0300

    Доделать 1

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+// int m = Convert.ToInt32(Console.ReadLine()), n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[3, 2];
+Random rnd = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 2; i++)
+    {
+        array[i, j] = Math.Round(rnd.NextDouble() * 10, 2);
+    }
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 2; i++)
+    {
+        Console.WriteLine(array[i, j]);
+    }
+    Console.WriteLine();
+}
